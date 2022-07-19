63bea0c8ed376bcd9a3e7560023c6fb5a228e1d4
denis <denisvictorjosphine1996@gmail.com>
2022-07-19 17:04:45 +0530

    do exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+return arr.sort((a,b)=>{
+if(a.hasOwnProperty('yearOfDeath')===false){
+ return (new Date().getFullYear()-a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)?-1:1;
+}
+if(b.hasOwnProperty('yearOfDeath')===false){
+    return (a.yearOfDeath-a.yearOfBirth)>(new Date().getFullYear()-b.yearOfBirth)?-1:1;
+   }
+return (a.yearOfDeath-a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)?-1:1;
+
+})[0];
 
 };
 
